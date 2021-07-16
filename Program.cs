ebde5b8d3296f7a4959e3921b0bec6e7ce6c3f2c
DerzkeyBK <43850433+DerzkeyBK@users.noreply.github.com>
2021-07-17 01:45:26 +0300

    final one

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xabe.FFmpeg;
+using Xabe.FFmpeg;//if this throws error just reinstall it
 
 namespace BadApple
 {
